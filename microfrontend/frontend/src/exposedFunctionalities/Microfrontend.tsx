3c39fff1e423cd39d9882e9150813c5127a68f43
Norbert Kruk <norbert.kruk@amartus.com>
2023-05-02 16:19:29 +0200

    Introduced props validation on the microfrontend level

--- microfrontend/frontend/src/exposedFunctionalities/Microfrontend.tsx
+++ microfrontend/frontend/src/exposedFunctionalities/Microfrontend.tsx
@@ -3,8 +3,23 @@ import type { HomePageProps } from "../HomePage";
 
 export type MicrofrontendProps = HomePageProps;
 
-// TODO Norbert: Add props validation
+const checkPropsValidity = ({ userIsPremium }: MicrofrontendProps): boolean => {
+  if (userIsPremium === undefined || typeof userIsPremium !== "boolean") {
+    return false;
+  }
+
+  return true;
+};
+
 const Microfrontend = ({ userIsPremium }: MicrofrontendProps) => {
+  const propsAreValid = checkPropsValidity({ userIsPremium });
+
+  if (!propsAreValid) {
+    throw new Error(
+      "Could not render microfrontend. Check if all required parameters are being passed."
+    );
+  }
+
   return <HomePage userIsPremium={userIsPremium} />;
 };
 
