db8adac546b11438cb7df5eee9cd52554650e620
Norbert Kruk <norbert.kruk@amartus.com>
2023-05-04 09:14:53 +0200

    Enhanced typescript support for the theme

--- microfrontend/frontend/src/theme/theme.ts
+++ microfrontend/frontend/src/theme/theme.ts
@@ -1,12 +1,12 @@
-import { extendTheme } from "@chakra-ui/react";
+import { ChakraTheme, extendTheme } from "@chakra-ui/react";
 import cardTheme from "./cardTheme";
 import dividerTheme from "./dividerTheme";
 
-const theme = extendTheme({
+const theme: Partial<ChakraTheme> = {
   components: {
     Card: cardTheme,
     Divider: dividerTheme,
   },
-});
+};
 
-export default theme;
+export default extendTheme(theme);
